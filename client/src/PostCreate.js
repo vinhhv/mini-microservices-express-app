5a395340949782429d723073628de610fa50a437
Vinh Vu <vinhhv@uci.edu>
2024-06-11 21:00:52 -0700

    Create post page

---
+++ client/src/PostCreate.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+export default function PostCreate() {
+  const [title, setTitle] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios.post("http://localhost:4000/posts", {
+      title,
+    });
+
+    setTitle("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+}
